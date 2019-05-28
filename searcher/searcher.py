81cb74c23e93bf365b27cac4d141723e4c68f3ce
fmndantas <fernandodantas1996@gmail.com>
2019-05-27 21:40:12 -0300

    general updates

--- searcher/searcher.py
+++ searcher/searcher.py
@@ -4,53 +4,57 @@ import pdb
 
 __EXT__ = 'py'
 
-def get_pack_dir(__file__):
+def _get_pack_dir(__file__):
     return Path(__file__).parent
     
-def get_subpackages(path):
+def _get_subpackages(path):
     return [dir for dir in os.listdir(path) if os.path.isdir(os.path.join(path, dir))]
 
-def get_modules(path, ext=''):
+def _get_modules(path, ext=''):
     files = [file for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
     filtered_files = list(filter(lambda file: file.split('.')[-1] in ext, files))
     return list(map(lambda file: file.split('.')[0], filtered_files))
     
-def get_functions(module):
+def _get_functions(module):
     pass
 
-def dsearch(name, path, dresults):
-    subpackages = get_subpackages(path)  # getting subpackages in current path
+def _dsearch(name, path, dresults):
+    subpackages = _get_subpackages(path)  # getting subpackages in current path
     if subpackages:  # if there are any subpackages
         for subpackage in subpackages:  # for all subpackages
-            if subpackage.find(name) > -1:  # if name was identified in subpackages
+            if subpackage.find(name) > -1 or name.find(subpackage) > -1:  # if name was identified in subpackages
                 dresults.append(os.path.join(path, subpackage))  # append path+subpackage to results
-            dsearch(name, os.path.join(path, subpackage), dresults)  # run recursive dsearch for current subpackage
+            _dsearch(name, os.path.join(path, subpackage), dresults)  # run recursive dsearch for current subpackage
     return dresults  # after all dsearch executions, return results with all possible matches for name
 
-def msearch(name, path, mresults):
-    modules = get_modules(path, __EXT__)
-    to_dirs = get_subpackages(path)
+def _analyze_match(name, tocomp):
+    # todo replace name.find(x) with analyze_match
+    # todo define levels of precision in mathing
+    pass
+
+def _msearch(name, path, mresults):
+    modules = _get_modules(path, __EXT__)
+    to_dirs = _get_subpackages(path)
     if modules:
         for module in modules:
-            if module.find(name) > -1:
+            if name.find(module) > -1 or module.find(name) > -1:
                 mresults.append(os.path.join(path, module))
     if to_dirs:
         for dir in to_dirs:
-            msearch(name, os.path.join(path, dir), mresults)
+            _msearch(name, os.path.join(path, dir), mresults)
     return mresults
 
-def format_result(libname, path, mode):
+def _format_result(libname, path):
     splitted = path.split(os.sep)
     desired_rslt = ''
-    if mode in ('subpackage', 'module'):
-        for split in splitted[splitted.index(libname):-1]:
-            desired_rslt += split+'.'
-        desired_rslt += splitted[-1]
+    for split in splitted[splitted.index(libname):-1]:
+        desired_rslt += split+'.'
+    desired_rslt += splitted[-1]
     return desired_rslt
 
-def all_functions(package):
+def getfunc(module):
     try:
-        return package.__all__
+        return module.__all__
     except AttributeError:
         return None
 
@@ -66,24 +70,15 @@ def search(name, package):
     -------
     sresults: all subpackages in which name appears
     mresults: all modules (.py files) in which name appears
-
-    Example of usage
-    ----------------
-    import numpy
-    from searcher import searcher
-    sresults, _ = searcher.search('matrix', numpy)
-
-    # Outputs
-    sresults = 'numpy.matrixlib'
     """
     sresults, mresults = [], []
-    path = get_pack_dir(package.__file__)
-    sresults = dsearch(name, path, sresults)
-    mresults = msearch(name, path, mresults)
+    path = _get_pack_dir(package.__file__)
+    sresults = _dsearch(name, path, sresults)
+    mresults = _msearch(name, path, mresults)
     if sresults:
         for pos, sresult in enumerate(sresults):
-            sresults[pos] = format_result(package.__name__.split('.')[0], sresult, mode='subpackage')
+            sresults[pos] = _format_result(package.__name__.split('.')[0], sresult)
     if mresults:
         for pos, mresult in enumerate(mresults):
-            mresults[pos] = format_result(package.__name__.split('.')[0], mresult, mode='module')
+            mresults[pos] = _format_result(package.__name__.split('.')[0], mresult)
     return sresults, mresults
--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setuptools.setup(
     version="0.1.0",
     author="Fernando Dantas",
     license="MIT",
-    description="Find anything in a package",
+    description="Find almost anything in a package",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url='https://github.com/fmndantas/searcher',
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
